a482991dec41d5bc2829b7c0d8b2faedea30d987
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-02 00:38:07 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,8 +1,8 @@
 def meal_choice(name = "meat")
+print "#{name}""
 end
 
-${name}  
-  
+
   
   # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
